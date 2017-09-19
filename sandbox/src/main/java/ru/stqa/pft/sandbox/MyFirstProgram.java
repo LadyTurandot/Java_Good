64e28df818e8dcc590123a4c91546305589920ba
LadyTurandot <princessturandot2016@gmail.com>
2017-09-19 18:56:39 +0300

    создан конструктор в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,12 +8,9 @@ public class MyFirstProgram {
 
         System.out.println("This is my first program");
 
-        Point p1 = new Point ();
-        Point p2 = new Point ();
-       p1.x = 4;
-       p1.y = 9;
-       p2.x = 16;
-       p2.y = 25;
+        Point p1 = new Point (4,9);
+        Point p2 = new Point (16,25);
+       
         System.out.println ("Расстояние между точками с координатами: " + p1.x + ";" + p1.y + " и " + p2.x + ";" + p2.y + " = " + distance(p1, p2));
     }
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,7 +2,13 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-        public double x;
-        public double y;
+    public double x;
+    public double y;
 
+
+    public Point(double x, double y) {
+        this. x = x;
+        this. y = y;
+
+    }
 }
