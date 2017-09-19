286923328a8fa63404a40809d250c32325dcb176
LadyTurandot <princessturandot2016@gmail.com>
2017-09-19 19:13:40 +0300

    реализовано вычисление при помощи метода в класее Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,12 +11,9 @@ public class MyFirstProgram {
         Point p1 = new Point (4,9);
         Point p2 = new Point (16,25);
        
-        System.out.println ("Расстояние между точками с координатами: " + p1.x + ";" + p1.y + " и " + p2.x + ";" + p2.y + " = " + distance(p1, p2));
+        System.out.println ("Расстояние между точками с координатами: " + p1.x + ";" + p1.y + " и " + p2.x + ";" + p2.y + " = " + p1.distance(p2));
     }
 
-    public static double distance (Point p1, Point p2) {
-        return Math.sqrt(((p1.x-p2.x)*(p1.x-p2.x))+((p1.y-p2.y)*(p1.y-p2.y)));
-    }
 }
 
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -11,4 +11,7 @@ public class Point {
         this. y = y;
 
     }
+    public double distance (Point p2) {
+        return Math.sqrt(((this.x-p2.x)*(this.x-p2.x))+((this.y-p2.y)*(this.y-p2.y)));
+    }
 }
