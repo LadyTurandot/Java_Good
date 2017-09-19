01996e786aebdd34ffdf1469c8ccfefe0cc82d20
LadyTurandot <princessturandot2016@gmail.com>
2017-09-19 14:48:17 +0300

    создан класс Point, создана функция distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,24 @@
 package ru.stqa.pft.sandbox;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class MyFirstProgram {
 
     public static void main(String[] args) {
 
         System.out.println("This is my first program");
+
+        double x = 4;
+        double y = 9;
+        double x1 = 16;
+        double y1 = 25;
+        System.out.println ("Расстояние между точками с координатами: " + x + ";" + y + " и " + x1 + ";" + y1 + " = " + distance(x, y, x1, y1));
+    }
+
+    public static double distance (double x, double y, double x1, double y1) {
+        return Math.sqrt(((x-x1)*(x-x1))+((y-y1)*(y-y1)));
     }
 }
+
+
+
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,7 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+
+        double x;
+        double y;
+}
