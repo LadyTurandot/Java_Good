e2370b8346debc4ec882851e2ab4b9298e4fb5fd
LadyTurandot <princessturandot2016@gmail.com>
2017-09-28 22:25:03 +0300

    В класс He;perBase перенесены вспомогательные методы класса ContactHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -4,47 +4,40 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model1.ContactData;
 
-public class ContactHelper {
-    private FirefoxDriver wd;
+public class ContactHelper extends HelperBase{
 
     public ContactHelper(FirefoxDriver wd) {
-        this. wd = wd;
+        super (wd);
     }
+
 
     public void returnToHomePage() {
         wd.findElement(By.linkText("home page")).click();
     }
 
     public void fillContactForm(ContactData contactData) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-        wd.findElement(By.name("mobile")).click();
-        wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+        type (By.name("firstname"), contactData.getFirstname());
+        type (By.name("lastname"), contactData.getLastname());
+        type (By.name("mobile"),contactData.getMobile());
+        type (By.name("email"), contactData.getEmail());
     }
 
     public void acceptAlertSwitch() {
+
         wd.switchTo().alert().accept();
     }
 
     public void selectContact() {
         if (!wd.findElement(By.name("selected[]")).isSelected()) {
-            wd.findElement(By.name("selected[]")).click();
+            click (By.name("selected[]"));
         }
     }
 
     public void returnToHome() {
-        wd.findElement(By.linkText("home")).click();
+       click (By.linkText("home"));
     }
 
     public void scriptDeletingPath() {
-        wd.findElement(By.xpath("//div[@id='content']/form[2]/div[2]/input")).click();
+        click (By.xpath("//div[@id='content']/form[2]/div[2]/input"));
     }
 }
