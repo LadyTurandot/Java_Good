9e7d4fc3a960c766b9523a36e99327775a61f9f9
LadyTurandot <princessturandot2016@gmail.com>
2017-09-29 11:34:54 +0300

    Создан класс GroupMidificaton (без использования рекодера)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -25,18 +25,30 @@ public class GroupHelper extends HelperBase{
     }
 
     public void initGroupCreation() {
+
         click(By.name("new"));
     }
 
     public void deleteSelectedGroups() {
+
         click(By.name("delete"));
     }
 
     public void selectGroup() {
+
         click(By.name("selected[]"));
     }
 
     public void scriptSubmitionPath() {
+
         click(By.xpath("//div[@id='content']/form/input[21]"));
+    }
+
+    public void submitGroupModification() {
+        click(By.name("update"));
+    }
+
+    public void initGroupModification() {
+        click(By.name("edit"));
     }
 }
