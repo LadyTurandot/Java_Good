db3fd1cdbfcd4212deb1e43073900cfc2522a853
LadyTurandot <princessturandot2016@gmail.com>
2017-10-04 13:07:36 +0300

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -4,18 +4,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(WebDriver wd) {
-       super (wd);
+        super(wd);
     }
 
     public void gotoGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
+
     public void gotoAddNewPage() {
-        wd.findElement(By.linkText("add new")).click();
+        if (isElementPresent(By.id("addaddressbookentry"))){
+            return;
+        }
+        click(By.linkText("add new"));
     }
 }
 
