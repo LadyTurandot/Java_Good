a64f1b4a1a620c9ab125c28ae83a20cbeda8274d
LadyTurandot <princessturandot2016@gmail.com>
2017-10-11 17:26:41 +0300

    Руализовано сравнение списков контактов целиком: до и после удаления контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -22,6 +22,11 @@ public class ContactDeletionTests extends TestBase {
         app.getContactHelper().returnToHome();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() -1);
+            Assert.assertEquals(before, after);
+
+        }
     }
 
-}
+
