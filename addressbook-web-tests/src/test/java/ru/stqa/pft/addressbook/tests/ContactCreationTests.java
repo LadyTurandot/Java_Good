aae380dec7b169018df8b682d8763209aeec34cc
LadyTurandot <princessturandot2016@gmail.com>
2017-10-12 15:15:41 +0300

    Сравнение списка контактов с сипользованием лямбда-выражения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model1.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -17,15 +18,8 @@ public class ContactCreationTests extends TestBase {
         app.getContactHelper().createContact(contact, true);
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
-
-
-        int max = 0;
-        for (ContactData g : after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-        contact.setId(max);
+        
+        contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(contact);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
