0e1b076faa5cd09fccb0ec591252654ceaef5f84
LadyTurandot <princessturandot2016@gmail.com>
2017-10-25 17:18:31 +0300

    Реализован анализ опций комвндрой строки при запуске генератора (контакты)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model1.ContactData;
 import ru.stqa.pft.addressbook.model1.GroupData;
 
@@ -12,16 +15,30 @@ import java.util.List;
 
 public class ContactDataGenerator {
 
+    @Parameter (names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter (names = "-f", description = "Target file")
+    public String file;
+
     public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
-
-        List<ContactData> contacts = generateContacts(count);
-        save(contacts, file);
-
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
     }
 
-    private static void save(List<ContactData> contacts, File file) throws IOException {
+    private void run() throws IOException {
+        List<ContactData> contacts = generateContacts(count);
+        save(contacts,  new File(file));
+    }
+
+    private void save(List<ContactData> contacts, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (ContactData contact : contacts) {
@@ -30,7 +47,7 @@ public class ContactDataGenerator {
         writer.close();
     }
 
-    private static List<ContactData> generateContacts(int count) {
+    private List<ContactData> generateContacts(int count) {
         List<ContactData> contacts = new ArrayList<ContactData>();
         for (int i = 0; i < count; i++) {
             contacts.add(new ContactData().withFirstname(String.format("FirstNameContact %s", i))
