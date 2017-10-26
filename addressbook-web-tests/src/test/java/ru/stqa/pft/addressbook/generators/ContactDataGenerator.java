60d4bb910fc8514557652c9ece328b26c63b121d
LadyTurandot <princessturandot2016@gmail.com>
2017-10-26 09:38:51 +0300

    Реализован генератор тестовых данных для контактов, сохраняющий данные в формате xml

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -3,8 +3,8 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model1.ContactData;
-import ru.stqa.pft.addressbook.model1.GroupData;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -21,6 +21,10 @@ public class ContactDataGenerator {
     @Parameter (names = "-f", description = "Target file")
     public String file;
 
+    @Parameter (names = "-d", description = "Data format")
+    public String format;
+
+
     public static void main(String[] args) throws IOException {
         ContactDataGenerator generator = new ContactDataGenerator();
         JCommander jCommander = new JCommander(generator);
@@ -35,10 +39,25 @@ public class ContactDataGenerator {
 
     private void run() throws IOException {
         List<ContactData> contacts = generateContacts(count);
-        save(contacts,  new File(file));
+        if (format.equals("csv")) {
+            saveAsCsv(contacts,  new File(file));
+        }else if (format.equals("xml")){
+            saveAsXml(contacts,  new File(file));
+        }else {
+            System.out.println("Unrecognized format" + format);
+        }
     }
 
-    private void save(List<ContactData> contacts, File file) throws IOException {
+    private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        String xml = xstream.toXML(contacts);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+    }
+
+    private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (ContactData contact : contacts) {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -21,18 +21,18 @@ public class ContactCreationTests extends TestBase {
 
     @DataProvider
     public Iterator<Object[]> validContacts() throws IOException {
-        List<Object[]> list = new ArrayList<Object[]>();
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
         String line = reader.readLine();
         while (line != null) {
-            String[] split = line.split(";");
-            list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1]).withMobile(split[2]).withEmail(split[3]).withGroup(split[4])});
+            xml += line;
             line = reader.readLine();
         }
-        return list.iterator();
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((g)-> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
-
-
 
     /*@DataProvider
     public Iterator<Object[]> validContacts() {
