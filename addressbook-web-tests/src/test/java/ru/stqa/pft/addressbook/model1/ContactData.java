56e049b41cf38a7e58ce499f4d3f77c2c1396b13
LadyTurandot <princessturandot2016@gmail.com>
2017-11-01 14:09:41 +0300

    Изменено кол-во сравниваемых атрибутов при проверке контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model1/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model1/ContactData.java
@@ -224,7 +224,9 @@ public class ContactData {
 
         if (id != that.id) return false;
         if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
-        return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
+        if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null) return false;
+        if (mobile != null ? !mobile.equals(that.mobile) : that.mobile != null) return false;
+        return email != null ? email.equals(that.email) : that.email == null;
     }
 
     @Override
@@ -232,6 +234,8 @@ public class ContactData {
         int result = id;
         result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
         result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+        result = 31 * result + (mobile != null ? mobile.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
         return result;
     }
 }
