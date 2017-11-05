73169be31abc13bd10ebf6f36891eaae8e08497c
LadyTurandot <princessturandot2016@gmail.com>
2017-11-05 19:33:02 +0300

    Реализовано отключение капчи удаленно по протоколу FTP

--- mantis-tests1/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests1/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -21,6 +21,7 @@ public class ApplicationManager {
 
     private String browser;
     private RegistrationHelper registrationHelper;
+    private FtpHelper ftp;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -51,6 +52,13 @@ public class ApplicationManager {
             registrationHelper =  new RegistrationHelper(this);
         }
         return registrationHelper;
+    }
+
+    public FtpHelper ftp() {
+        if (ftp == null){
+            ftp = new FtpHelper(this);
+        }
+        return ftp;
     }
 
     public WebDriver getDriver() {
--- mantis-tests1/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests1/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -5,6 +5,9 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
+import java.io.IOException;
+
 
 public class TestBase {
 
@@ -16,10 +19,12 @@ public class TestBase {
     @BeforeSuite
     public void setUp() throws Exception {
         app.init();
+        app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
     }
 
     @AfterSuite
-    public void tearDown() {
+    public void tearDown() throws IOException {
+        app.ftp().restore("config_inc.php.bak", "config_inc.php");
         app.stop();
     }
     }
