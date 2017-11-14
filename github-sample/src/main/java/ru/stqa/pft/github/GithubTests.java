f7bb45f930280ee541f45a3e4074124cc4ea726c
LadyTurandot <princessturandot2016@gmail.com>
2017-11-14 17:55:11 +0300

    Реализовано взаимодействие с GitHub

---
+++ github-sample/src/main/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("8421b1e7a4ed1fb773391f5fc54d412fbb347af7");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("LadyTurandot", "Java_Good")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
